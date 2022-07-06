033a592e12d107d189885381b344c5157710054c
Christian Romero <christianromero.work@gmail.com>
2022-07-06 13:45:37 -0500

    Added html elements to index.html based on pseudocode; added variables and prototyping to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/*
+Define variables to link html elements
+
+Timer function
+
+Start button click event listener
+    Function that hides start button and renders question and answers element,
+    starts timer
+
+Correct answer function that progresses quiz and adds to score
+
+Incorrect answer function that decrements timer by fixed amount
+
+Click event listener for answers
+
+Function that populates question element
+
+Function that populates answer elements
+*/
+
+var startButton = document.querySelector("#start-button");
+var quizQuestion = document.querySelector("#quiz-question");
+var quizAnswers = document.querySelector("quiz-answers");
+var startButton = document.querySelector("#start-button");
+
+console.log(startButton);
